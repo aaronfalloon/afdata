a72718a44965356339d6428d4e54f3ae21cbd9b7
Aaron Falloon <contact@aaronfalloon.com>
2017-02-01 12:50:42 +0000

    Add a license and a setup.py file for creating a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='afdata',
+    description='A simple data science library',
+    version='0.0.1',
+    url='https://github.com/aaronfalloon/afdata',
+    author='Aaron Falloon',
+    license='MIT',
+    packages=['afdata']
+)
