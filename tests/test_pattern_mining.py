efe6c987e17150ce427524198148e69449d0326b
Aaron Falloon <contact@aaronfalloon.com>
2017-02-02 12:51:48 +0000

    Update tests for previous commit

--- tests/test_pattern_mining.py
+++ tests/test_pattern_mining.py
@@ -13,23 +13,24 @@ transactions = [
 
 class PatternMining(unittest.TestCase):
     def test_returns_support_for_itemset_milk_bread(self):
-        support = pattern_mining.support(['milk', 'bread'], transactions)
+        support = pattern_mining.support(frozenset(['milk', 'bread']), transactions)
 
         self.assertEqual(support, 2 / 7)
 
     def test_returns_support_for_itemset_bread(self):
-        support = pattern_mining.support(['bread'], transactions)
+        support = pattern_mining.support(frozenset(['bread']), transactions)
 
         self.assertEqual(support, 5 / 7)
 
     def test_returns_0_for_support_when_itemset_not_in_transactions(self):
-        support = pattern_mining.support(['bread', 'tea'], transactions)
+        support = pattern_mining.support(frozenset(['bread', 'tea']), transactions)
 
         self.assertEqual(support, 0)
 
     def test_returns_confidence_for_rule_if_milk_then_bread(self):
         confidence = pattern_mining.confidence(
-            ['milk'], ['bread'],
+            frozenset(['milk']),
+            frozenset(['bread']),
             transactions
         )
 
@@ -37,8 +38,8 @@ class PatternMining(unittest.TestCase):
 
     def test_returns_confidence_for_rule_if_bread_then_butter_jam(self):
         confidence = pattern_mining.confidence(
-            ['bread'],
-            ['butter', 'jam'],
+            frozenset(['bread']),
+            frozenset(['butter', 'jam']),
             transactions
         )
 
@@ -46,8 +47,8 @@ class PatternMining(unittest.TestCase):
 
     def test_returns_0_confidence_when_union_of_itemsets_has_0_support(self):
         confidence = pattern_mining.confidence(
-            ['bread'],
-            ['butter', 'jam', 'tea'],
+            frozenset(['bread']),
+            frozenset(['butter', 'jam', 'tea']),
             transactions
         )
 
@@ -55,8 +56,8 @@ class PatternMining(unittest.TestCase):
 
     def test_returns_0_for_confidence_when_itemset_a_has_0_support(self):
         confidence = pattern_mining.confidence(
-            ['bread', 'tea'],
-            ['butter', 'jam'],
+            frozenset(['bread', 'tea']),
+            frozenset(['butter', 'jam']),
             transactions
         )
 
